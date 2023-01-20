a3503833356d91ee0ea1deedd07017e99a742f42
ignacio-001 <tsk7558@gmail.com>
2023-01-20 13:50:05 +0530

    added landing page and signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './client/landingpage/landingpage.component';
+import { SignupformComponent } from './client/signupform/signupform.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingpageComponent, SignupformComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
